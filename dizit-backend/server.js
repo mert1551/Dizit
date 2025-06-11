5abbc6228607609c1a9730bf49ba95a8b5617514
mert1551 <neonbir28@gmail.com>
2025-06-12 00:35:40 +0300

    Update server.js

--- dizit-backend/server.js
+++ dizit-backend/server.js
@@ -16,10 +16,12 @@ const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cors())
-app.use(express.static(path.join(__dirname, "../")))
-app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, '..', 'index.html'))
-})
+app.use(express.static(path.join(__dirname, '..')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'index.html'));
+});
+
 
 // MongoDB bağlantısı
 app.use(express.json({ limit: '200kb' }));
