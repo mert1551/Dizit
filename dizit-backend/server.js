f50ec375813284f59d958860064eb0da9296cd21
mert1551 <neonbir28@gmail.com>
2025-06-17 10:47:32 +0300

    Update server.js

--- dizit-backend/server.js
+++ dizit-backend/server.js
@@ -25,7 +25,7 @@ app.get('/', (req, res) => {
 
 // MongoDB bağla
 app.use(cors())
-app.use(express.static(path.join(__dirname, "/")))
+app.use(express.static(path.join(__dirname, ".")))
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "../index.html"))
 })
